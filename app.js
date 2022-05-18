ce7127b8202b81b89c6926b6529a2f9c8fbb1752
Thomas Vergara Gonzalez <tomasvergara844@gmail.com>
2022-05-18 15:04:11 -0500

    Se llaman las rutas de proveedores

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.static (__dirname + "/public"))
 app.use('/', require('./router/RutasWeb'))
 app.use('/productos', require('./router/Productos'))
 app.use('/clientes', require('./router/Clientes'))
+app.use('/proveedores', require('./router/Proveedores'))
 
 app.use((req, res, next) =>{
     res.status(404).render("404", {
