5831093d735c29b74b34337da030603c9ff5b39d
Thomas Vergara Gonzalez <tomasvergara844@gmail.com>
2022-05-18 15:58:58 -0500

    se desarrollo que llame los datos de provedores desde la BD

--- router/Proveedores.js
+++ router/Proveedores.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) =>{
-    res.render("proveedores", {
-        arrayProveedores: [
-            {idProveedor: 1, nombre: "proveedor_uno", telefono: 11111111, direccion: "direccion-proveedor1", ciudad: "ciudad-proveedor1"},
-            {idProveedor: 2, nombre: "proveedor_dos", telefono: 222222222, direccion: "direccion-proveedor2", ciudad: "ciudad-proveedor2"}
-        ]
-    })
+const Proveedor = require('../models/proveedor')
+
+router.get('/', async(req, res) =>{
+
+    try {
+        const arrayProveedoresDB = await Proveedor.find()
+        console.log(arrayProveedoresDB)
+
+        res.render("proveedores", {
+            arrayProveedores : arrayProveedoresDB
+            /* [
+                {idProveedor: 1, nombre: "proveedor_uno", telefono: 11111111, direccion: "direccion-proveedor1", ciudad: "ciudad-proveedor1"},
+                {idProveedor: 2, nombre: "proveedor_dos", telefono: 222222222, direccion: "direccion-proveedor2", ciudad: "ciudad-proveedor2"}
+            ] */
+        })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router;
