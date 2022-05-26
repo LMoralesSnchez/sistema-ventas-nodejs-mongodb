9860ca76d534d8aea84fc3b85e2eb1a3e206be3e
Thomas Vergara Gonzalez <tomasvergara844@gmail.com>
2022-05-26 05:59:02 -0500

    Se agrego la ruta de la vista de agregar venta y la ruta para guardar una venta en bd

--- router/Ventas.js
+++ router/Ventas.js
@@ -21,4 +21,22 @@ router.get('/', async(req, res) =>{
     }
 })
 
+router.get('/agregar-venta', (req,res)=>{
+    res.render('agregar-venta')
+})
+
+router.post('/', async(req, res) =>{
+    const body = req.body
+    console.log(body)
+    try {
+        const ventaDB = new Venta(body)
+        await ventaDB.save()
+        console.log(ventaDB)
+
+        res.redirect('/ventas')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 module.exports = router
